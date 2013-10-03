6e0703b599aaa05b03f1e36c7d0286135177932e
Jon Cohen <jonathandcohen222@gmail.com>
2013-10-03 16:43:48 -0400

    Couldn't push.  Made a fresh clone and re-added constants and the databaseDoc.

---
+++ FastFactsDB/dbConstants.h
@@ -0,0 +1,29 @@
+//
+//  dbConstants.h
+//  MobileFastFacts
+//
+//  Created by Jon Cohen on 10/3/13.
+//  Copyright (c) 2013 Duquesne University. All rights reserved.
+//
+
+#ifndef MobileFastFacts_dbConstants_h
+#define MobileFastFacts_dbConstants_h
+
+enum {
+    NUMBER = 0,
+    NAME,
+    AUTHOR,
+    COMMUNICATION,
+    CORE_CURRICULUM,
+    ETHICS_LAW_POLICY_HEALTH_SYSTEMS,
+    GERIATRICS,
+    NON_PAIN_SYMPTOMS_SYNDROMES,
+    PAIN_EVALUATION,
+    PAIN_NON_OPIOIDS,
+    PAIN_OPIOIDS,
+    PEDIATRICS,
+    PROGNOSIS,
+    PSYCHOSOCIAL_SPIRITUAL_EXPERIENCE
+};
+
+#endif
