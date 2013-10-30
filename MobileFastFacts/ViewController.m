b514f986f16ae87a2f77a82bba6e43a62603079d
James Block <blockj1@duq.edu>
2013-10-29 23:13:08 -0400

    Homepage in the works. Early Stage Push. -Bill McDowell

---
+++ MobileFastFacts/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  MobileFastFacts
+//
+//  Created by JMAMacUser on 10/29/13.
+//  Copyright (c) 2013 Duquesne University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    
+     [self.navigationItem setHidesBackButton:YES];
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
