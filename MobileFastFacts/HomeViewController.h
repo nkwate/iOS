7c64b67985429655f0e0407574a7874acb57c39b
James Block <blockj1@duq.edu>
2013-11-03 14:37:13 -0500

    Fixed icon and made homepage not display a back option.

---
+++ MobileFastFacts/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  MobileFastFacts
+//
+//  Created by JMAMacUser on 11/3/13.
+//  Copyright (c) 2013 Duquesne University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *Articles;
+- (IBAction)pushd:(id)sender;
+
+@end
